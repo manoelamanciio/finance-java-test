9c78699b09a98e54ed3400922ff215dcf7536608
Manoel <manoel.amanciio@gmail.com>
2026-08-21 21:12:39 -0300

    test: stabilize integration test infrastructure

--- src/test/java/tech/artadevs/finances/AbstractIntegrationTest.java
+++ src/test/java/tech/artadevs/finances/AbstractIntegrationTest.java
@@ -1,12 +1,15 @@
 package tech.artadevs.finances;
 
+import java.time.Duration;
+
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.PostgreSQLContainer;
 
 public abstract class AbstractIntegrationTest {
 
-    static final PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:16-alpine");
+    static final PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:16-alpine")
+            .withStartupTimeout(Duration.ofMinutes(3));
 
     static {
         postgres.start();
